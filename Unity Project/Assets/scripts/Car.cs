5b1334a69acfbf645dcf320062d2636a3dd00ab4
Arashi1017 <55470682+Arashi1017@users.noreply.github.com>
2019-11-20 12:02:01 +0800

    程式

---
+++ Unity Project/Assets/scripts/Car.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    //定義欄位(設定變數)
+    //語法寫法：
+    //修飾詞+類型+名稱+(指定值)+結束(;)
+      //修飾詞:私人private(預設)、公開public
+
+    //類型:
+    //整數int (指定值:=值)
+   public int cc =2000;
+    //浮點數float (=值f)
+   public float weight =150.5f;
+    //字串string (="字串")
+   public string brand ="BMW";
+    //布林值bool (=true/false)
+   public bool _break =true;
+
+    //屬性attribute
+    [Header("年代")]
+   public int _bc;
+    [Range(10,300)]
+   public float speed;
+    [Tooltip("這是車車的顏色")]
+   public string color;
+}
---
+++ Unity Project/Assets/scripts/player.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public string run ="跑步開關";
+}
